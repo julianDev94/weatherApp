b5780f14111396816701f33051127d94e1609765
JulianAballay <juliancabj1210@gmail.com>
2025-11-08 01:48:05 -0300

    agrego componente condicional que muestra un mensaje si en el estado ciudad hay una cadena vacia caso contrario va mostrar el componente displayClima

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,42 @@ import FormularioConsulta from "./components/FormularioConsulta";
 import TituloPrincipal from "./components/TituloPrincipal";
 import ClimaDisplay from "./components/ClimaDisplay";
 import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 import { useEffect, useState } from "react";
-
-
 
 function App() {
   const [clima, setClima] = useState({});
   const [ciudad, setCiudad] = useState("");
+  
 
-  useEffect(()=>{
+  useEffect(() => {
     consultaAPI();
-  },[]);
+  }, [ciudad]);
 
-  const consultaAPI = async () =>{
+  const consultaAPI = async () => {
     const apiKey = "278f9f00026e70711f4fffe4a00476a1";
-    const respuesta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&lang=es`);
+    const respuesta = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&lang=es`
+    );
     const datosBody = await respuesta.json();
-    console.log(datosBody);
     setClima(datosBody);
-  }
+  };
+
+  const displayClima = !ciudad ? (
+    <Container className="text-center my-5">
+      <h4>🌦️ Ingrese una ciudad para consultar el clima</h4>
+      <p>Por ejemplo: "Buenos Aires", "Madrid" o "Tucumán"</p>
+    </Container>
+  ) : (
+    <ClimaDisplay clima={clima} />
+  );
 
   return (
     <>
       <section className="seccionPrincipal">
         <TituloPrincipal />
         <FormularioConsulta setCiudad={setCiudad} />
-        <ClimaDisplay clima={clima} />
+        {displayClima}
       </section>
 
       <Footer></Footer>
