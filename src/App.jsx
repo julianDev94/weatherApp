0da5b7c6405367f58089104b0d972fc00d9f3f93
JulianAballay <juliancabj1210@gmail.com>
2025-11-08 00:21:30 -0300

    agrego el estado clima para almacenar el objeto obtenido de la API y el estado ciudad para obtener del formulario el nombre de la ciudad y poder hacer una peticion del clima a esa ciudad

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,31 @@ import FormularioConsulta from "./components/FormularioConsulta";
 import TituloPrincipal from "./components/TituloPrincipal";
 import ClimaDisplay from "./components/ClimaDisplay";
 import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
+
+
+
 function App() {
+  const [clima, setClima] = useState({});
+  const [ciudad, setCiudad] = useState("");
+
+  useEffect(()=>{
+    // consultaAPI();
+  },[]);
+
+  const consultaAPI = async () =>{
+    const apiKey = "278f9f00026e70711f4fffe4a00476a1";
+    const respuesta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Tucuman&appid=${apiKey}&lang=es`);
+    const datosBody = await respuesta.json();
+    console.log(datosBody);
+    setClima(datosBody);
+  }
+
   return (
     <>
       <section className="seccionPrincipal">
         <TituloPrincipal />
-        <FormularioConsulta />
+        <FormularioConsulta setCiudad={setCiudad} />
         <ClimaDisplay />
       </section>
 
