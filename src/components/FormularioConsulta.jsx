fcfe76cee33fa89ab5b84e2fea56deb49dcf60ad
JulianAballay <juliancabj1210@gmail.com>
2025-11-07 22:13:00 -0300

    agrego posicion de las columnas en tamañao sm para que sea responsive el formulario

--- src/components/FormularioConsulta.jsx
+++ src/components/FormularioConsulta.jsx
@@ -7,7 +7,7 @@ const FormularioConsulta = () => {
       <Container>
         <Form className="py-3">
           <Row>
-            <Col md={8}>
+            <Col sm={12} md={8}>
               <Form.Group className="mb-3" controlId="busquedaClimaCiudad">
                 <Form.Control
                   type="text"
@@ -15,11 +15,11 @@ const FormularioConsulta = () => {
                   minLength={3}
                   maxLength={20}
                   required
-                  className="mx-3"
+                  
                 />
               </Form.Group>
             </Col>
-            <Col md={2}>
+            <Col sm={12} md={2}>
               <Button variant="dark" type="submit">
                  <i className="bi bi-search"></i> Buscar
               </Button>
