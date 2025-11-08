73712915531780ceed7b3cc557a701faeb739356
JulianAballay <juliancabj1210@gmail.com>
2025-11-07 22:29:59 -0300

    agrego link de google fonts para cambiar la fuente del titulo del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import FormularioConsulta from "./components/FormularioConsulta";
-
+import TituloPrincipal from "./components/TituloPrincipal";
 function App() {
   return (
     <section>
+      <TituloPrincipal />
       <FormularioConsulta />
     </section>
   );
---
+++ src/components/TituloPrincipal.jsx
@@ -0,0 +1,11 @@
+const TituloPrincipal = () => {
+  return (
+    <div>
+      <h1 className="text-center titulo text-white mb-0 py-3">
+        Clima al instante
+      </h1>
+    </div>
+  );
+};
+
+export default TituloPrincipal;
