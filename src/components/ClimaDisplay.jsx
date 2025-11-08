2ac2bd38ec168a3af3a08ca5a5266dd74bf10ab4
JulianAballay <juliancabj1210@gmail.com>
2025-11-08 01:19:27 -0300

    modifico un poco el diseño del maquetado del display

--- src/components/ClimaDisplay.jsx
+++ src/components/ClimaDisplay.jsx
@@ -1,23 +1,26 @@
 import { Container, Card } from "react-bootstrap";
 
 const ClimaDisplay = ({ clima }) => {
-  let temperaturaCelsius = clima.main?.temp ? (clima.main.temp - 273.15).toFixed(1)
+  let temperaturaCelsius = clima.main?.temp
+    ? (clima.main.temp - 273.15).toFixed(1)
     : null;
-  let sensacionTermina = clima.main?.feels_like ? (clima.main?.feels_like - 273.15).toFixed(1) : null;
+  let sensacionTermina = clima.main?.feels_like
+    ? (clima.main?.feels_like - 273.15).toFixed(1)
+    : null;
   return (
     <Container>
       <section className="my-5">
         <h3>Tiempo de {clima.name}</h3>
         <Container className="colorNoche text-white d-flex flex-column justify-content-center">
-          <p>{clima.weather?.[0]?.description}</p>
-          <div className="d-flex">
+          <p className="text-center">{clima.weather?.[0]?.description}</p>
+          <div className="d-flex justify-content-evenly">
             <div>
               <p>Temperatura: {temperaturaCelsius}</p>
               <span>Sensacion Termica: {sensacionTermina}</span>
             </div>
             <div>
-              {/* <p>{clima.main.pressure}</p>
-              <span>{clima.main.humidity}%</span> */}
+              <p>Presion atmosferica: {clima.main?.pressure}</p>
+              <span>Humedad: {clima.main?.humidity}%</span>
             </div>
           </div>
         </Container>
