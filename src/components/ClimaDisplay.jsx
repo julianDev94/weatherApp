88404799e50706849ef9e3f936c9a15a886d9aff
JulianAballay <juliancabj1210@gmail.com>
2025-11-08 01:32:03 -0300

    agrego unos detalles al display agregandole iconos significativos para cada propiedad

--- src/components/ClimaDisplay.jsx
+++ src/components/ClimaDisplay.jsx
@@ -9,18 +9,18 @@ const ClimaDisplay = ({ clima }) => {
     : null;
   return (
     <Container>
-      <section className="my-5">
+      <section className="my-5 ">
         <h3>Tiempo de {clima.name}</h3>
-        <Container className="colorNoche text-white d-flex flex-column justify-content-center">
-          <p className="text-center">{clima.weather?.[0]?.description}</p>
+        <Container className="colorNoche text-white d-flex flex-column justify-content-center ">
+          <p className="text-center fs-2 fw-bold"><i className="bi bi-cloud-moon-fill"></i> {clima.weather?.[0]?.description}</p>
           <div className="d-flex justify-content-evenly">
             <div>
-              <p>Temperatura: {temperaturaCelsius}</p>
-              <span>Sensacion Termica: {sensacionTermina}</span>
+              <p className="fs-3 fw-bold"><i className="bi bi-thermometer"></i>{temperaturaCelsius}°</p>
+              <span>Sensación de {sensacionTermina}°</span>
             </div>
             <div>
-              <p>Presion atmosferica: {clima.main?.pressure}</p>
-              <span>Humedad: {clima.main?.humidity}%</span>
+              <p>Presión atmosférica de {clima.main?.pressure}</p>
+              <span>Humedad de {clima.main?.humidity}%</span>
             </div>
           </div>
         </Container>
