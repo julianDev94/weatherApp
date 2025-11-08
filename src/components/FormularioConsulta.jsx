8d2ae836bae862ac47690e72fc41766070defc9c
JulianAballay <juliancabj1210@gmail.com>
2025-11-07 21:32:00 -0300

    agrego las validaciones para el input

--- src/components/FormularioConsulta.jsx
+++ src/components/FormularioConsulta.jsx
@@ -4,8 +4,8 @@ const FormularioConsulta = () => {
   return (
     <section className="bg-secondary">
       <Form>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Control type="email" placeholder="Buscar por ciudad" />
+        <Form.Group className="mb-3" controlId="busquedaClimaCiudad">
+          <Form.Control type="text" placeholder="Buscar por ciudad" minLength={3} maxLength={20} required />
         </Form.Group>
         <Button variant="dark" type="submit">
           Buscar
