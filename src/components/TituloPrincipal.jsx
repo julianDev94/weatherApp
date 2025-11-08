504f61004140cc9990b51a5ee7fa1fbbd234bc0c
JulianAballay <juliancabj1210@gmail.com>
2025-11-07 22:31:17 -0300

    quito el div innecesario del titulo

--- src/components/TituloPrincipal.jsx
+++ src/components/TituloPrincipal.jsx
@@ -1,10 +1,8 @@
 const TituloPrincipal = () => {
   return (
-    <div>
-      <h1 className="text-center titulo text-white mb-0 py-3">
-        Clima al instante
-      </h1>
-    </div>
+    <h1 className="text-center titulo text-white mb-0 py-3">
+      Clima al instante
+    </h1>
   );
 };
 
