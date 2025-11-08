259650ee7884d2ba2ac9ea8cc11fc4c4f00d8588
JulianAballay <juliancabj1210@gmail.com>
2025-11-07 23:04:09 -0300

    agrego el maquetado del componente footer con los agradecimientos a la API de openWeather

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import FormularioConsulta from "./components/FormularioConsulta";
 import TituloPrincipal from "./components/TituloPrincipal";
 import ClimaDisplay from "./components/ClimaDisplay";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <TituloPrincipal />
       <FormularioConsulta />
       <ClimaDisplay />
+      <Footer></Footer>
     </>
   );
 }
